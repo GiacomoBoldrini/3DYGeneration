0f5d262f77c80458614ace55263c22c4a02bedb8
Giacomo Boldrini <gboldrin@cern.ch>
2025-04-04 14:52:19 +0200

    litttle fix

--- runners/2018/chain_step_2_test.sh
+++ runners/2018/chain_step_2_test.sh
@@ -26,6 +26,7 @@ if [ "${CMSSW_RELEASE}" != "local" ]; then
 
 fi
 
-python ${RUN_DIR}/modifyCfg.py ${RUN_DIR}/SMP-RunIISummer20UL18HLT-00035_1_cfg.py ${RUN_DIR}/step_2_cfg.py --events=20 --randomSeeds=${SEED}
+#python ${RUN_DIR}/modifyCfg.py ${RUN_DIR}/SMP-RunIISummer20UL18HLT-00035_1_cfg.py ${RUN_DIR}/step_2_cfg.py --events=20 --randomSeeds=${SEED}
+mv ${RUN_DIR}/SMP-RunIISummer20UL18HLT-00035_1_cfg.py ${RUN_DIR}/step_2_cfg.py
 
 cmsRun -e -j FrameworkJobReport.xml ${RUN_DIR}/step_2_cfg.py
